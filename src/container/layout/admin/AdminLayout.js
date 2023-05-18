b6c5a8f232b3e43deee8e82e51bb07d2a6a7bd10
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2023-05-19 01:27:00 +0700

    [UI][Sidebar] init Sidebar

--- src/container/layout/admin/AdminLayout.js
+++ src/container/layout/admin/AdminLayout.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminHeader from "./AdminHeader";
+import SidebarWithHeader from "./SidebarWithHeader";
 
 const AdminLayout = () => {
   return (
     <div className="min-h-[100vh] h-[100vh] w-[100%] flex flex-col">
       <div className="w-[100%] h-[100%] overflow-y-auto">
-        <AdminHeader></AdminHeader>
+        <SidebarWithHeader></SidebarWithHeader>
         <Outlet />
       </div>
     </div>
