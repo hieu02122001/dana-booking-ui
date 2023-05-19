ac0d68e57b05767ddced2520c877c8b54617aa08
Trung Hiếu Văn <trunghieuvan02@gmail.com>
2023-05-19 13:10:33 +0700

    [UI][Footer] add Footer component

--- src/container/layout/admin/AdminLayout.js
+++ src/container/layout/admin/AdminLayout.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import AdminHeader from "./AdminHeader";
 import AdminSidebarWithHeader from "./AdminSidebarWithHeader";
+import Footer from "../Footer";
 
 const AdminLayout = () => {
   return (
     <div className="min-h-[100vh] h-[100vh] w-[100%] flex flex-col">
       <div className="w-[100%] h-[100%] overflow-y-auto">
-        <AdminSidebarWithHeader></AdminSidebarWithHeader>
-        <Outlet />
+        <AdminSidebarWithHeader>
+          <Outlet />
+        </AdminSidebarWithHeader>
       </div>
+      <Footer />
     </div>
   );
 };
